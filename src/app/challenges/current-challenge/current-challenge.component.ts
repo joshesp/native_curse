a4973d0c0b2448cae551ab2f1096fa8c0efad614
Josh <me@me.com>
2019-07-16 19:39:22 -0500

    firs commit

---
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ns-current-challenge',
+    templateUrl: './current-challenge.component.html',
+    styleUrls: ['./current-challenge.component.css'],
+    moduleId: module.id
+})
+export class CurrentChallengeComponent {
+    challengedDescription = '';
+    currentChallenge = '';
+
+    onSetChallenge() {
+        this.currentChallenge = this.challengedDescription;
+    }
+}
